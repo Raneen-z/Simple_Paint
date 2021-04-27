979089227c74b18332f519fe4a74ae779b7e0f89
Raneen-z <raneen.alzafarani@tuwaiq.edu.sa>
2021-04-28 00:52:02 +0300

    upload solution for rec and circle

--- SimplePaint/Form1.Designer.cs
+++ SimplePaint/Form1.Designer.cs
@@ -32,19 +32,20 @@ namespace SimplePaint
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -58,6 +59,10 @@ namespace SimplePaint
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1050, 563);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
             // groupBox1
             // 
@@ -71,6 +76,45 @@ namespace SimplePaint
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Shapes";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(227, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 56);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "◯";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(162, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 56);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "/";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(77, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "▯";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label6);
@@ -81,6 +125,32 @@ namespace SimplePaint
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tools";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(166, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 56);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "🖐";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(86, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 56);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "✎";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // groupBox3
             // 
@@ -96,89 +166,50 @@ namespace SimplePaint
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Pen Style";
             // 
-            // label1
+            // label10
             // 
-            this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(77, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 56);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "▯";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.AutoSize = true;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(167, 41);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 40);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Color";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click_1);
             // 
-            // label3
+            // numericUpDown1
             // 
-            this.label3.AutoSize = true;
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(223, 27);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 56);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "▯";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.numericUpDown1.Location = new System.Drawing.Point(282, 52);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(66, 31);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
-            // label2
+            // label9
             // 
-            this.label2.AutoSize = true;
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(162, 27);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 56);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "/";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(227, 27);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 56);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "◯";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(86, 27);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 56);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "✎";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(166, 27);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(76, 56);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "🖐";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(33, 27);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(52, 56);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "...";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(282, 24);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 25);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Size";
             // 
             // label8
             // 
@@ -191,34 +222,32 @@ namespace SimplePaint
             this.label8.TabIndex = 6;
             this.label8.Text = "ـــ";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
-            // label9
+            // label7
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(282, 24);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(43, 25);
-            this.label9.TabIndex = 7;
-            this.label9.Text = "Size";
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(33, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 56);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "...";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
-            // numericUpDown1
+            // label3
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(282, 52);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(66, 31);
-            this.numericUpDown1.TabIndex = 8;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label10.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label10.Location = new System.Drawing.Point(167, 41);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(85, 40);
-            this.label10.TabIndex = 9;
-            this.label10.Text = "Color";
-            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(223, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 56);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "▯";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -260,6 +289,7 @@ namespace SimplePaint
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- SimplePaint/Form1.cs
+++ SimplePaint/Form1.cs
@@ -7,16 +7,242 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace SimplePaint
 {
     public partial class Form1 : Form
     {
+        private string shapeName = "line";
+        private bool solidStyle = true;
+        private bool drawMood = true;
+        private int fontSize = 7;
+        private bool isClicked = false;
+        private Point location1; // starting location
+        private Point location2; // ending location
+        Rectangle rec = new Rectangle(); // object of rectangle
+        private Shape currentShape = null;
+        private List <Shape> shapes = new List<Shape> { };
+        Pen pen;
+        Graphics g;
+
+
+        public class Shape
+        {
+            public Point location1, location2;
+            public string shapeName;
+            public Pen pen;
+
+
+        }
         public Form1()
         {
+
             InitializeComponent();
+            this.g = this.CreateGraphics();
+            this.pen = new Pen(Color.Black,this.fontSize);
+
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.shapeName = "rectangle";
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.shapeName = "line";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            this.shapeName = "circle";
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (this.solidStyle)
+            {
+                this.pen.DashStyle = DashStyle.DashDot;
+                this.solidStyle = !this.solidStyle; //dotted
+            }
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (!this.solidStyle)
+            {
+                this.pen.DashStyle = DashStyle.Solid;
+                this.solidStyle = !this.solidStyle; //solid
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.fontSize = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0)); //get the size
+            this.pen.Width = this.fontSize;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (!this.drawMood) this.drawMood = !this.drawMood; //select mood
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (this.drawMood) this.drawMood = !this.drawMood; //draw mood
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.pen.Color = colorDialog1.Color; //choose color for the pen
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (this.drawMood)
+            {
+                this.isClicked = !this.isClicked; //true
+                this.location1 = e.Location; // get starting location
+                this.currentShape = new Shape();
+                this.currentShape.location1 = this.location1;
+            }
+            switch (this.shapeName)
+            {
+                case "line":
+                    this.currentShape.shapeName = this.shapeName;
+                    break;
+                case "rectangle":
+                    this.currentShape.shapeName = this.shapeName;
+                    break;
+                case "circle":
+                    this.currentShape.shapeName = this.shapeName;
+                    break;
+            }
+            
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (this.isClicked && this.drawMood )
+            {
+                this.location2 = e.Location; // get the ending location of x and y 
+                this.isClicked = !this.isClicked; //make it false and wait for new point entry
+                this.currentShape.location2 = this.location2;
+            }
+            this.currentShape.pen = this.pen;
+            this.shapes.Add(this.currentShape);
+            Refresh();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            /*if (this.isClicked && this.drawMood)
+            {
+                this.location2 = e.Location; //get the current location 
+            }
+            Refresh(); // refresh the form
+*/
+
+        }
+
+        private Rectangle GetRect(Point location1, Point location2)
+        {
+            this.rec = new Rectangle();
+            this.rec.X = Math.Min(location1.X, location2.X); // x-value
+            this.rec.Y = Math.Min(location1.Y, location2.Y); // y-value
+            this.rec.Width = Math.Abs(location1.X - location2.X);
+            this.rec.Height = Math.Abs(location1.Y - location2.Y);
+            return this.rec;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(var shape in shapes)
+            {
+                switch (shape.shapeName)
+                {
+                    case "line":
+                        //
+                        break;
+                    case "rectangle":
+                        e.Graphics.DrawRectangle(shape.pen, GetRect(shape.location1,shape.location2));
+                        break;
+                    case "circle":
+                        e.Graphics.DrawEllipse(shape.pen, GetRect(shape.location1, shape.location2));
+                        break;
+                }
+            }
+          
+               
+        }
+
+        private void label10_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.pen.Color = colorDialog1.Color; //choose color for the pen
+        }
     }
 }
+
+
+/*
+ 
+{
+  public class Line
+  {
+    public Point start;
+    public Point end;
+  }
+  public partial class Form1 : Form
+  {
+    private int positoinOffset = 0;
+    private int x1 =0, y1=0;
+    private int x2 =0, y2=0;
+    private bool isClicked = false;
+    private List<Line> lines;
+    private Line currentLine = null;
+    public Form1()
+    {
+      InitializeComponent();
+      this.lines = new List<Line>();
+    }
+    private void Form1_MouseClick(object sender, MouseEventArgs e)
+    {
+      if (!this.isClicked)
+      {
+        this.currentLine = new Line();
+        currentLine.start = new Point(e.X, e.Y);
+      }
+      else
+      {
+        currentLine.end = new Point(e.X, e.Y);
+        this.lines.Add(currentLine);
+        this.Invalidate();
+      }
+      this.isClicked = !isClicked;
+    }
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics g = e.Graphics;
+      Pen p = new Pen(Brushes.Black, 4);
+      p.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+      foreach (var line in this.lines)
+        g.DrawLine(p, 
+        line.start.X,
+        line.start.Y,
+        line.end.X,
+        line.end.Y);
+    }
+    private void button1_Click(object sender, EventArgs e)
+    {
+      this.positoinOffset += 32;
+      this.Invalidate();
+    }
+  }
+} 
+*/
